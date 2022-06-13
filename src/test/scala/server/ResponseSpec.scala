c6b5585e534780fcf3a3299b31603d29bc0e32fb
Leandro Galvan <leusgalvan@gmail.com>
2022-06-13 10:26:24 -0300

    Improve response spec

--- src/test/scala/server/ResponseSpec.scala
+++ src/test/scala/server/ResponseSpec.scala
@@ -3,10 +3,21 @@ package server
 class ResponseSpec extends munit.FunSuite {
   test("Response is correctly encoded to bytes") {
     val response = Response(
-      httpVersion = "HTTP/1.1", status = 200, body = "{\"id\":123}".getBytes, headers = Map("Content-Length" -> "10")
+      httpVersion = "HTTP/1.1",
+      status = 200,
+      body = "{\"id\":123}".getBytes,
+      headers = Map(
+        "Content-Type" -> "application/json",
+        "Content-Length" -> "10"
+      )
     )
     val encoded = new String(response.bytes)
-    val expected ="HTTP/1.1 200 OK\nContent-Length: 10\r\n\r\n{\"id\":123}"
+    val expected =
+      "HTTP/1.1 200 OK\n" +
+      "Content-Type: application/json\r\n" ++
+      "Content-Length: 10\r\n" ++
+      "\r\n" ++
+      "{\"id\":123}"
     assertEquals(encoded, expected)
   }
 }
