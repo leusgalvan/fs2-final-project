3815973dd9c8eeb1f1621282a59b6ddad50cc4f2
Leandro Galvan <leusgalvan@gmail.com>
2022-06-13 10:34:34 -0300

    Add request spec

---
+++ src/test/scala/server/RequestSpec.scala
@@ -0,0 +1,25 @@
+package server
+
+class RequestSpec extends munit.FunSuite {
+  test("Request should calculate correct content length when it has the header") {
+    val request = Request(
+      method = "POST",
+      url = "/api",
+      httpVersion = "HTTP/1.1",
+      headers = Map("Content-Length" -> "15"),
+      body = Array.empty
+    )
+    assertEquals(request.contentLength, 15)
+  }
+
+  test("Request should calculate content length 0 when it has no header") {
+    val request = Request(
+      method = "POST",
+      url = "/api",
+      httpVersion = "HTTP/1.1",
+      headers = Map(),
+      body = Array.empty
+    )
+    assertEquals(request.contentLength, 0)
+  }
+}
